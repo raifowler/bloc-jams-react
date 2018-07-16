66a5f0f0db125c7a7bbed95e8dd8019679659777
Raimond Fowler <raifowler@gmail.com>
2018-07-15 20:10:31 -0700

    Found typo in setSong() function that was causing the function not to work. Also added e.preventDefault to onClick method so single click on new song changes song.

--- src/components/Album.js
+++ src/components/Album.js
@@ -31,7 +31,7 @@ class Album extends Component {
   }
 
   setSong(song) {
-    this.auddioElement.src = song.audioSrc;
+    this.audioElement.src = song.audioSrc;
     this.setState({ currentSong: song });
   }
 
@@ -65,7 +65,7 @@ class Album extends Component {
           <tbody>
             {
               this.state.album.songs.map( (song, index) =>
-                <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                <tr className="song" key={index} onClick={(e) => {e.preventDefault(); this.handleSongClick(song)}} >
                   <td>{index + 1}</td>
                   <td>{song.title}</td>
                   <td>{song.duration}</td>
